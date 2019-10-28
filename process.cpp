6205385b54934de9ceca5a5f8ecbca3c48e02117
omerfarukbasekin <56569869+omerfarukbasekin@users.noreply.github.com>
2019-10-28 22:33:07 +0300

    Create process.cpp

---
+++ process.cpp
@@ -0,0 +1,41 @@
+    scanf("%d", &sayi);
+ 
+       if(sayi < 0)
+    {
+          printf("Lutfen pozitif bir sayı girin \n");
+      scanf("%d", &sayi);
+    }
+ 
+    pid = fork();
+ 
+  
+ 
+     if(pid == 0)  //Child process
+    {
+             do
+        {
+        if(sayi%2 != 0)
+        {
+          sayi = (sayi*3)+1;
+                }
+         
+        else if(sayi%2 == 0)
+        {
+          sayi = sayi/2;
+        }
+ 
+        printf("%d \n",sayi); // önce child process lar yazdırılıyor
+        }while(sayi != 1);
+    }
+ 
+    else  //Parentprocess
+    {
+ 
+        printf("pid %d \n",pid);
+    printf("Child görevi bitmesi bekleniyor. \n"); // wait fonk yardımı ile
+      wait(&status);
+      }     
+     
+     
+return 0;   
+}
